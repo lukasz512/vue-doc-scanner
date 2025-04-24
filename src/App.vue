7a78100f3fcae55acfc688161640a81ef0ec908a
Lukasz Ostrowski <hello@lukasz-ostrowski.pl>
2025-04-24 20:09:06 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div :class="`theme-${theme}`">
+    <router-view />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue';
+
+const theme = ref(localStorage.getItem('theme') || (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'));
+onMounted(() => {
+  document.documentElement.className = theme.value === 'dark' ? 'dark' : 'light';
+});
+</script>
