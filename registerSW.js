2b084f74b8a153c374775ef992839fcdf9f129e3
lukasz512 <lukasz512@users.noreply.github.com>
2025-04-24 18:42:20 +0000

    deploy: 34893025d3c5ceccee676c9eaebe79a5c642c4ac

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/vue-doc-scanner/sw.js', { scope: '/vue-doc-scanner/' })})}
