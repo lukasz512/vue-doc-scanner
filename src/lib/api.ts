31393f88374f1f217db4758e94b94f5e9f518ca3
Lukasz Ostrowski <lukasz512@gmail.com>
2025-04-25 21:15:30 +0000

    init

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,13 +2,16 @@ const API_URL = 'https://script.google.com/macros/s/AKfycbxNI_qyVD1FiyyKFnGPnWjm
 
 export async function uploadFile(base64: string, filename: string): Promise<boolean> {
   try {
-    const formData = new FormData()
-    formData.append('file', base64)
-    formData.append('filename', filename)
+    const params = new URLSearchParams()
+    params.append('file', base64)
+    params.append('filename', filename)
 
     const response = await fetch(API_URL, {
       method: 'POST',
-      body: formData
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: params
     })
 
     if (!response.ok) {
